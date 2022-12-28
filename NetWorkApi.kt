a61e6b2c4370379b3c911e422ea5569d2670ce62
agustiawan1717 <102985184+agustiawan1717@users.noreply.github.com>
2022-12-28 19:06:15 +0700

    Add files via upload

---
+++ NetWorkApi.kt
@@ -0,0 +1,12 @@
+package com.agustiawan.demoproject
+
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface NetWorkApi{
+
+    @GET("/")
+    fun getProducts(): Call<List<ProductFamily>>
+
+}
